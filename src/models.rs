f5e7b9c5eb446f30576b4f6dd75183ef86c176a8
Loakesh I <loakesh.indiran@wowlabz.com>
2024-01-01 15:45:20 +0530

    add: models & db layer for jira

---
+++ src/models.rs
@@ -0,0 +1,58 @@
+use std::collections::HashMap;
+
+use serde::{Serialize, Deserialize};
+
+// TODO: derive the appropriate traits
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed
+}
+
+// TODO: derive the appropriate traits
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![]
+        }
+    }
+}
+
+// TODO: derive the appropriate traits
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+// TODO: derive the appropriate traits
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct DBState {
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>
+}
